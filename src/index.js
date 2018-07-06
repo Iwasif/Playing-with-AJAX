12041a35ac23d36c24a93b5c8452e0da82e2295e
wasifmiraz <wasifmiraz@mail.com>
2018-07-06 11:20:26 +0600

    event json data show and hide

--- src/index.js
+++ src/index.js
@@ -6,8 +6,20 @@ let btn = document.querySelector('#loadData')
 
 let p = document.querySelector('#output')
 
+let isShown = false
+
+p.style.visibility='visible'
+btn.innerHTML='show'
+
+
 
 btn.addEventListener('click', () =>{
+
+if(isShown)
+{
+     p.style.visibility = 'hidden'
+     isShown = false
+    btn.innerHTML='show'
     const xhr = new  XMLHttpRequest()
 
     xhr.onreadystatechange = () => {
@@ -17,4 +29,13 @@ p.innerHTML =xhr.response;
 
     xhr.open('GET', URL)
     xhr.send()
+}
+
+else {
+    p.style.visibility = 'visible'
+ isShown =true
+btn.innerHTML='hide'
+
+}
+
 })
