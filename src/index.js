5696362266cf015d0a015e955f9054366820cad2
wasifmiraz <wasifmiraz@mail.com>
2018-07-06 10:29:47 +0600

    update index js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './index.css'
+
+const URL = 'http://localhost:3000/contacts'
+
+const xhr = new  XMLHttpRequest()
+
+xhr.onreadystatechange = () => {
+    console.log(xhr.response)
+
+}
+
+xhr.open('GET', URL)
+xhr.send()
